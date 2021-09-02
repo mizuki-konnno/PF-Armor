e501d9667f01442619ed50c93cc1bdca7e53087e
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-02 01:00:28 +0000

    [Add]ログイン関連

---
+++ db/migrate/20210902003206_add_name_to_users.rb
@@ -0,0 +1,9 @@
+class AddNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :gender, :integer, default: 0, null: false
+    add_column :users, :height, :integer, default: 0
+    add_column :users, :introduction, :text
+    add_column :users, :profile_image, :string
+  end
+end
