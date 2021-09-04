4c9bb7980470de4dc025d1e6cf553345a9a79b38
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-04 07:24:40 +0000

    [Add]views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'cordes#index'
+  
+  resources :clothes, only: [:new, :create, :destroy, :index, :show]
+  resources :genres, only: [:new, :create, :destroy, :index]
+  resources :cordes, only: [:new, :create, :destroy, :index, :show]
+  resources :wish_lists, only: [:new, :create, :destroy, :index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
 end
