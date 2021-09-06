3077d95fa9cd151bf8c8e60601a7080a8c26f60f
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-06 11:24:09 +0000

    [Add]user関連

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :cordes, dependent: :destroy
   has_many :wish_list, dependent: :destroy
   has_many :clothes, dependent: :destroy
+
+  enum gender: { 男性: 0, 女性: 1 }
+  attachment :profile_image
 end
