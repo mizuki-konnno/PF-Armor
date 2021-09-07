bb914729e23c89df4b26250524f2c6e3ad14f0c6
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-07 07:32:29 +0000

    [Update]Clothe関連

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,2 +1,10 @@
 class ClothesController < ApplicationController
+  def index
+    @clothes = current_user.clothes
+  end
+
+  def new
+    @clothe = Clothe.new
+  end
+
 end
--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -2,4 +2,7 @@ class Clothe < ApplicationRecord
 
   belongs_to :user
   belongs_to :genre
+
+  attachment :clothe_image
+
 end
