cfb985569b0b0a2083d705c7a98670998487901d
mizuki-konnno <m.m.mizuki.0215@gmail.com>
2021-09-08 01:31:01 +0000

    [Update]Clothe関連

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -5,6 +5,27 @@ class ClothesController < ApplicationController
 
   def new
     @clothe = Clothe.new
+    @genres = current_user.genres
+  end
+
+  def show
+    @clethe = Clothe.find(params[:id])
+  end
+
+  def create
+    @clothe = Clothe.new(clothe_params)
+    @clothe.user_id = current_user.id
+    @clothe.save
+    redirect_to clothes_path
+  end
+
+  def destroy
+  end
+
+  private
+
+  def clothe_params
+    params.require(:clothe).permit(:title, :brand, :clothes_image, :caption)
   end
 
 end
